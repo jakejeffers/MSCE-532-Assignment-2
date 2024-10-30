93add7a4ce180341d181dc32691f86ff80987654
Jacob Jeffers <jake.jeffers@ucumberlands.edu>
2024-10-29 23:57:04 -0400

    Quick Sort Algorithm

---
+++ MSCE532-Assignment2.py
@@ -0,0 +1,43 @@
+import random
+import time
+import sys
+from memory_profiler import memory_usage
+
+# Quick Sort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
+# Merge Sort
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result.extend(left[i:])
+    result.extend(right[j:])
+    return result
+# Test datasets
+def generate_datasets(size):
+    sorted_data = list(range(size))
+    reverse_sorted_data = sorted_data[::-1]
+    random_data = [random.randint(0, size) for _ in range(size)]
+    return sorted_data, reverse_sorted_data, random_data
